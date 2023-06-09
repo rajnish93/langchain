ebbb0b8628120b7596d3afeb24fefa060fde913e
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2023-06-09 20:15:22 +0530

    langchain framwwork with openAPI and other model

---
+++ checkGPU.py
@@ -0,0 +1,7 @@
+import torch
+train_on_gpu = torch.cuda.is_available()
+
+if train_on_gpu:
+    print('CUDA is available, Training on GPU ...')
+else:
+    print('CUDA is not available!  Training on CPU ...')
